84c702f628c6867b09dbf06131330c6afb13cd02
Albert Cervantes <cervanalbert@gmail.com>
2020-04-06 22:12:15 -0700

    Removed comments

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+var memberType;
+const employeeArr = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const managerQuestions = [{
@@ -80,8 +83,6 @@ const internQuestions = [{
     message: "Which type of team member would you like to add?",
     choices: ["Intern","Engineer","I don't want to add any more team members"]
 }];
-var memberType;
-const employeeArr = [];
 
 inquirer
 .prompt(managerQuestions)
@@ -133,31 +134,9 @@ function printArray() {
         console.log(employeeArr[j]);
     }
 }
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// const html = render(employeeArr);
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 function writeHTMLFile(html){
     fs.writeFile(outputPath, html, error => {
         if(error){ return console.log(error); }
         console.log("Successfully Created team.html...");
     })
 }
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
